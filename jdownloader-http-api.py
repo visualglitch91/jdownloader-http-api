7cd3e2f2a3379f0303cab368a42993c59ac72d99
visualglitch91 <visualglitch91@proton.me>
2023-11-27 17:58:07 -0300

    Improve error handling

--- jdownloader-http-api.py
+++ jdownloader-http-api.py
@@ -13,74 +13,43 @@ port = config['port']
 device = None
 app = Flask(__name__)
 jd = myjdapi.Myjdapi()
-jd.connect(email, password)
-
-
-def update_device():
-    global device
-    try:
-        jd.update_devices()
-        device = jd.get_device(device_name)
-    except Exception as e:
-        device = None
-
 
 def get_device():
-    if not device:
-        update_device()
+    global device
+
+    if device is None:
+        jd.connect(email, password)
+        jd.update_devices()
+        device = jd.get_device(device_name)
+
     return device
-
-
-update_device()
-print(f"Device updated: {device}")
-
-
-def retry_operation(operation, *args, **kwargs):
-    max_retries = 2  # Maximum number of retries
-    retry_count = 0
-
-    while retry_count < max_retries:
-        try:
-            result = operation(*args, **kwargs)
-            return result  # If successful, return the result
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            update_device()  # Call update_device() to potentially fix the issue
-            retry_count += 1
-            print(f"Retrying... Attempt {retry_count}/{max_retries}")
-
-    # If max_retries reached without success, handle or raise an exception
-    print("Maximum retries reached. Operation failed.")
-    # Handle or raise an exception here
-    raise Exception("Maximum retries reached. Operation failed.")
 
 
 @app.route('/links', methods=['GET'])
 def get_links():
-    try:
-        def get_links_function():
-            return get_device().downloads.query_links()
+    global device
 
-        response = retry_operation(get_links_function)
+    try:
+        response = get_device().downloads.query_links()
         return jsonify(response)
     except Exception as e:
+        device = None
         return jsonify({'error': e}), 500
 
 
 @app.route('/links', methods=['POST'])
 def add_link():
+    global device
     data = request.json
     if 'url' in data:
         try:
-            def add_links_function():
-                return get_device().linkgrabber.add_links([{
-                    "autostart": True,
-                    "links": data.get('url'),
-                }])
-
-            new_link = retry_operation(add_links_function)
+            new_link = get_device().linkgrabber.add_links([{
+                "autostart": True,
+                "links": data.get('url'),
+            }])
             return jsonify(new_link), 201
         except Exception as e:
+            device = None
             return jsonify({'error': e}), 500
     else:
         return jsonify({'error': 'Invalid request'}), 400
@@ -88,15 +57,14 @@ def add_link():
 
 @app.route('/links/delete', methods=['POST'])
 def delete_link():
+    global device
     data = request.json
     if 'ids' in data:
         try:
-            def delete_links_function():
-                return get_device().downloads.remove_links(link_ids=data.get("ids"))
-
-            retry_operation(delete_links_function)
-            return jsonify({success: True}), 200
+            get_device().downloads.remove_links(link_ids=data.get("ids"))
+            return jsonify({"success": True}), 200
         except Exception as e:
+            device = None
             return jsonify({'error': e}), 500
     else:
         return jsonify({'error': 'Invalid request'}), 400
